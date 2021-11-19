82f61639caf0f93c924ddb1c4d330eb133259b63
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-20 03:11:12 +0400

    chore: add BypassFinalsHook

--- tests/Hook/BypassFinalsHook.php
+++ tests/Hook/BypassFinalsHook.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Zorachka\EventDispatcher\Tests\Hook;
+namespace Zorachka\Framework\Tests\Hook;
 
 use DG\BypassFinals;
 use PHPUnit\Runner\BeforeTestHook;
 
-final class BypassFinalHook implements BeforeTestHook
+final class BypassFinalsHook implements BeforeTestHook
 {
     public function executeBeforeTest(string $test): void
     {
